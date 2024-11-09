01a526148343145ebf5225d9b47a0c74b521858a
Linus Pummer <linus.pummer@web.de>
2024-11-09 20:35:00 +0100

    execution method added without tests

--- src/main/java/ChessObjects/Board.java
+++ src/main/java/ChessObjects/Board.java
@@ -1,9 +1,10 @@
 package ChessObjects;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Board {
-    private Piece [][] pieces;
+    public Piece [][] pieces;
     private ArrayList<Move> previousMoves;
 
     public Board(){
@@ -11,5 +12,16 @@ public class Board {
         previousMoves = new ArrayList<>();
     }
 
+    //position methods
+    public boolean isInsideBoard(Point position){
+        return position.x >= 0 && position.x < pieces.length && position.y >= 0 && position.y < pieces.length;
+    }
 
+    public boolean isOccupied(Point position){
+        if (!isInsideBoard(position)){
+            return false;
+        }
+
+        return pieces[position.y][position.x] != null;
+    }
 }
--- src/main/java/ChessObjects/Move.java
+++ src/main/java/ChessObjects/Move.java
@@ -6,19 +6,21 @@ public class Move {
     private final Piece movingPiece, capturedPiece;
     private final Point previousPosition, postponedPosition;
     private final Move connectedMove;
+    private final Board board;
 
     //general constructor
-    public Move(Piece movingPiece, Piece capturedPiece, Point previousPosition, Point postponedPosition, Move connectedMove) {
+    public Move(Piece movingPiece, Piece capturedPiece, Point postponedPosition, Move connectedMove, Board board) {
         this.movingPiece = movingPiece;
         this.capturedPiece = capturedPiece;
-        this.previousPosition = previousPosition;
+        this.previousPosition = movingPiece.currentPosition;
         this.postponedPosition = postponedPosition;
         this.connectedMove = connectedMove;
+        this.board = board;
     }
 
     //constructor for basic moves
-    public Move(Piece movingPiece, Point previousPosition, Point postponedPosition){
-        this(movingPiece, null, previousPosition, postponedPosition, null);
+    public Move(Piece movingPiece, Point postponedPosition, Board board){
+        this(movingPiece, null, postponedPosition, null, board);
     }
 
     //checks if a piece was captured by this move
@@ -27,10 +29,23 @@ public class Move {
     }
 
     public void execute(){
-        //Todo
+        removeCapturedPiece();
+        movingPiece.setPosition(postponedPosition);
+
+        if (connectedMove != null){
+            connectedMove.execute();
+        }
     }
 
     public void undo(){
         //Todo
+    }
+
+    //execution methods
+    public void removeCapturedPiece(){
+        if (isCaptureMove()) {
+            Point removePos = capturedPiece.currentPosition;
+            board.pieces[removePos.y][removePos.x] = null;
+        }
     }
 }
