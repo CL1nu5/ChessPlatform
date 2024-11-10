5562bf7d6fc50815132b045bbfeabcbd1182759c
Linus Pummer <linus.pummer@web.de>
2024-11-10 12:55:01 +0100

    undo method added, log in error case added, move class completed

--- src/main/java/ChessObjects/Move.java
+++ src/main/java/ChessObjects/Move.java
@@ -1,5 +1,8 @@
 package ChessObjects;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
 import java.awt.*;
 
 public class Move {
@@ -7,6 +10,8 @@ public class Move {
     private final Point previousPosition, postponedPosition;
     private final Move connectedMove;
     private final Board board;
+
+    private static final Logger logger = (Logger) LogManager.getLogger(Move.class);
 
     //general constructor
     public Move(Piece movingPiece, Piece capturedPiece, Point postponedPosition, Move connectedMove, Board board) {
@@ -30,7 +35,10 @@ public class Move {
 
     public void execute(){
         removeCapturedPiece();
-        movingPiece.setPosition(postponedPosition);
+
+        if(!movingPiece.setPosition(postponedPosition)){
+            logger.error("execute - position update not possible");
+        }
 
         if (connectedMove != null){
             connectedMove.execute();
@@ -38,7 +46,15 @@ public class Move {
     }
 
     public void undo(){
-        //Todo
+        if (connectedMove != null){
+            connectedMove.undo();
+        }
+
+        if (!movingPiece.setPosition(previousPosition)){
+            logger.error("undo - position update not possible");
+        }
+
+        addCapturedPiece();
     }
 
     //execution methods
@@ -46,6 +62,14 @@ public class Move {
         if (isCaptureMove()) {
             Point removePos = capturedPiece.currentPosition;
             board.pieces[removePos.y][removePos.x] = null;
+        }
+    }
+
+    //undo methods
+    public void addCapturedPiece(){
+        if (isCaptureMove()){
+            Point addPos = capturedPiece.currentPosition;
+            board.pieces[addPos.y][addPos.x] = capturedPiece;
         }
     }
 }
