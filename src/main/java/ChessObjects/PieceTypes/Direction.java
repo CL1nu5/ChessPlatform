154a0e9c9b4a694e2f844c9c8bb53728c2eee25a
Linus Pummer <linus.pummer@web.de>
2024-11-17 17:58:20 +0100

    added general move direction checker

--- src/main/java/ChessObjects/PieceTypes/Direction.java
+++ src/main/java/ChessObjects/PieceTypes/Direction.java
@@ -3,8 +3,11 @@ package ChessObjects.PieceTypes;
 import java.awt.*;
 
 public enum Direction {
+    /* linear directions */
     Up(-1, 0), Down(1, 0), Right(0, 1), Left(0, -1),
-    Up_Right(-1, 1), Up_Left(-1, -1), Down_Right(1, 1), Down_Left(1, -1);
+    Up_Right(-1, 1), Up_Left(-1, -1), Down_Right(1, 1), Down_Left(1, -1),
+    /* knight directions */
+    Up_Up_Right(-2, 1),;
 
     public final int x, y;
 
