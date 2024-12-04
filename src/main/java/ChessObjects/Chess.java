af0f4bbf42c0be28da01d2d923a636e0460f5d26
Linus Pummer <linus.pummer@web.de>
2024-12-04 18:43:25 +0100

    reworked chess class added more ege case tests@

--- src/main/java/ChessObjects/Chess.java
+++ src/main/java/ChessObjects/Chess.java
@@ -5,27 +5,41 @@ import java.awt.*;
 public class Chess {
 
     //translates (x,y) to (c,n)
-    public static String translatePosToCoords(Point position){
-        try {
-            String x = String.valueOf((char) (position.x + 97));
-            String y = String.valueOf(8 - position.y);
-            return x + y;
+    public static String translatePosToCoords(Point position) {
+        String x = String.valueOf((char) (position.x + 97));
+        String y = String.valueOf(8 - position.y);
+
+        if (position.x < 0 || position.x > 7) {
+            x = " ";
         }
-        catch (Exception e){
-            return null;
+        if (position.y < 0 || position.y > 7) {
+            y = " ";
         }
+
+        return x + y;
     }
 
     //translates (c,n) to (x,y)
-    public static Point translateCoordsToPos(String coordinates){
-        try {
-            String[] vals = coordinates.split("");
-            int x = (int) (vals[0].charAt(0)) - 97;
-            int y = 8 - Integer.parseInt(vals[1]);
-            return new Point(x, y);
-        }
-        catch (Exception e){
+    public static Point translateCoordsToPos(String coordinates) {
+        //get the 2 values
+        if (coordinates.length() != 2) {
             return null;
         }
+        String[] vals = coordinates.split("");
+
+        try {
+            int x = (int) (vals[0].charAt(0)) - 97;
+            int y = 8 - Integer.parseInt(vals[1]);
+
+            //x and y need to be between 0 and 7
+            if (x > 7 || x < 0 || y > 7 || y < 0) {
+                return null;
+            }
+
+            return new Point(x, y);
+        } catch (Exception e) {
+            return null;
+        }
+
     }
 }
